6ddc15c32119ed489b4062508b5d429be46c0efd
Yurii Mudryi <yurii.mudryi@elopage.com>
2019-04-26 16:25:04 +0300

    attempt to refactor

--- src/Components/Sidebar/SidebarList.js
+++ src/Components/Sidebar/SidebarList.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Collapse from "@material-ui/core/es/Collapse/Collapse";
 import List from "@material-ui/core/es/List/List";
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import {Typography} from '@material-ui/core/es/index';
+
 import SidebarItem from "./SidebarItem";
 
-export default ({items, selectLesson}) => (
-  <List component="div" disablePadding>
-    {items.map(item => (
-      <SidebarItem
-        button
-        children={item.children}
-        isCategory={item.isCategory}
-        key={item.id}
-        locked={item.locked}
-        name={item.name}
-        selectLesson={selectLesson}
-      />
-    ))}
-  </List>
-)
+const SidebarList = ({items, selectLesson}) => {
+  const [open, toggleOpen] = useState(false);
+
+  const ExpandIcon = open ? ExpandLess : ExpandMore;
+
+  return (
+    <List component="div" disablePadding>
+      {items.map(({children, isCategory, id, locked, name}) => (
+        <SidebarItem
+          key={id}
+          locked={locked}
+          name={name}
+          sidebarButtonChildren={isCategory ? <ExpandIcon/> : locked && <Typography color="error">Locked</Typography>}
+          handleClick={isCategory ? () => toggleOpen(!open) : () => selectLesson(name)}
+        >
+          {isCategory && (
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <SidebarList
+                items={children}
+                selectLesson={selectLesson}
+              />
+            </Collapse>
+          )}
+        </SidebarItem>
+      ))}
+    </List>
+  )
+};
+
+export default SidebarList
